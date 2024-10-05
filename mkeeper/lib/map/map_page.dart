a398d9ee8f5de7ed40548034160ebe2bf016c0b4
Jeong Jihyo <jihyo0331@gmail.com>
2024-10-05 23:40:21 +0900

    Update map_page.dart

--- mkeeper/lib/map/map_page.dart
+++ mkeeper/lib/map/map_page.dart
@@ -1,73 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
-void map() {
-  runApp(const Map());
-}
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
 
-class Map extends StatelessWidget {
-  const Map({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('main Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: const Center(
+        child: Text('main page'),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class Main extends StatefulWidget {
+  const Main({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MainState createState() => _MainState(); // createState 오타 수정
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      body: const Center(
+        child: Text('main widget'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
