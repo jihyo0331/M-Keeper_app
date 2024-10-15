aaed7d96c502619a6068c4bdb015896306531b92
Jeong Jihyo <jihyo0331@gmail.com>
2024-10-15 21:14:26 +0900

    add

---
+++ mkeeper/lib/map/favorites_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainPage(),  // MainPage를 첫 화면으로 설정
+    );
+  }
+}
+
--- mkeeper/lib/map/map_page.dart
+++ mkeeper/lib/map/map_page.dart
@@ -27,7 +27,7 @@ class _MapPageState extends State<MapPage> {
   void initState() {
     super.initState();
     _speak(
-        "이곳은 길찾기 페이지 입니다. 길찾기를 하려면 화면을 길게 누르시고 안내음이 나오면 목적지를 말씀해 주세요. 설명을 다시 듣고 싶으시면 화면을 한번 터치해 주세요.");
+        "이곳은 길찾기 페이지 입니다. 길찾기를 하려면 화면을 길게 누르시고, 안내음이 나오면 목적지를 말씀해 주세요. 설명을 다시 듣고 싶으시면 화면을 한번 터치해 주세요.");
   }
 
   Future<void> _playGuideVoice() async {
@@ -74,7 +74,7 @@ class _MapPageState extends State<MapPage> {
           ),
           onTap: () {
             _speak(
-                "이곳은 길찾기 페이지 입니다. 길찾기를 하려면 화면을 길게 누르시고 안내 음성이 나오면 목적지를 말씀해 주세요. 설명을 다시 듣고 싶으시면 화면을 한번 터치해 주세요.");
+                "이곳은 길찾기 페이지 입니다. 길찾기를 하려면 화면을 길게 누르시고, 안내음이 나오면 목적지를 말씀해 주세요. 설명을 다시 듣고 싶으시면 화면을 한번 터치해 주세요.");
           },
           onDoubleTap: () {
             Navigator.push(
